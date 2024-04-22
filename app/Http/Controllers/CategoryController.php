9778c51814d2dcaaf97351a3dae1e354feb5b597
new name <willys.el.trueno@gmail.com>
2024-04-21 21:09:22 -0400

    CRUD CATEGORIA

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Category\StoreCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+
+use function PHPUnit\Framework\isEmpty;
 
 class CategoryController extends Controller
 {
@@ -23,23 +26,45 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        // Create a new category
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        //VALIDAR DATOS
+        $category = $request->validated();
+        $category['slug'] = $this->createSlug($category['name']);
+        //GUARDAR EL REQUEST VALIDADO
+        Category::create($category);
+        //RETORNAR MENSAJE GUARDADO
+        return response()->json([
+            "message" => "La Categoria fue registrada..!!!",
+            "category" => $category,
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Category $category)
+    /* public function show(Category $category) */
+    public function show(string $term)
     {
-        //
+        $category = Category::where('id', $term)
+            ->orWhere('slug', $term)
+            ->get();
+        //VALIDAR QUE EXISTA LA CATEGORIA
+        if (count($category) == 0) {
+            return response()->json([
+                "message" => "No se encontro la categoria",
+            ], 404);
+        }
+        //RETORNANDO EL REGISTRO
+        return response()->json([
+            "category" => $category[0],
+        ]);
     }
 
     /**
@@ -53,18 +78,59 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, string $id)
     {
-        //
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                "message" => "No se encontro la categoria",
+            ], 404);
+        }
+
+        if ($request['name']) {
+            $request['slug'] = $this->createSlug($request['name']);
+        }
+
+        $category->update($request->all());
+        
+        //RETORNANDO RESPUESTA
+        return response()->json([
+            "message" => "La Categoria fue actualizada..!!!",
+            "category" => $category,
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category)
+    public function destroy(string $id)
     {
-        //
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json([
+                "message" => "No se encontro la categoria",
+            ], 404);
+        }
+
+        $category->delete();
+
+        return response()->json([
+            "message" => "La Categoria fue eliminada..!!!",
+            "category" => $category,
+        ], 200);
+    }
+
+    private function createSlug(string $text)
+    {
+        //convirtiendo a minusculas
+        $text = strtolower($text);
+        $text = preg_replace('/[^a-z0-9]+/', '-', $text);
+        //eliminando espacios
+        $text = trim($text, '-');
+        //si hay simbolo - se siguie manteniendo el guion
+        $text = preg_replace('/-+/', '-', $text);
+        return $text;
     }
 }
-
-
